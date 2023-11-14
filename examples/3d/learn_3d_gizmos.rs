50e0cd7162e3ed004fec8238da036bb149034576
Yangyang <181393422@qq.com>
2023-11-14 15:56:02 +0800

    Learn Sample

---
+++ examples/3d/learn_3d_gizmos.rs
@@ -0,0 +1,21 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0., 1.5, 6.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
